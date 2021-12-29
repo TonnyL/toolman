3bf73b71d8374b2ff194e7fa66ea488fcd92042e
tonnyl <lizhaotailang@gmail.com>
2021-12-29 11:56:38 +0800

    Fix check of existing rebase progress

--- rebase-all.sh
+++ rebase-all.sh
@@ -3,9 +3,12 @@
 origin_master=$(git rev-parse --abbrev-ref origin/head)
 current_branch=$(git symbolic-ref --short HEAD)
 
-branches=$(git branch --format='%(refname:short)')
+branches=$(git branch --format='%(refname:short)')\
 
-if [ $? == 1 ]; then
+# It will return exit code 0 (success) if there is a rebase folder, and it will output the rebase folder to STDOUT.
+tmp=$(ls `git rev-parse --git-dir` | grep rebase)
+
+if [ $? == 0 ]; then
   echo "git rebase in progress"
   exit 1
 fi
